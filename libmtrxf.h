c3f5e85509013ca7ab04da5e744d10124e7533a8
lionholler <82542096+lionholler@users.noreply.github.com>
2021-07-28 16:53:43 +0200

    version 280721

--- libmtrxf.h
+++ libmtrxf.h
@@ -3,12 +3,12 @@
 /* the size of matrix struct is  capped  at *
  * 256 x 256 by default. This value can  be *
  * tweeked when bigger  matrices  are  used */
-#define MAX_WIDTH_MATRIX 256
-#define MAX_HEIGHT_MATRIX 256
+#define MAX_WIDTH_MATRIX 	256
+#define MAX_HEIGHT_MATRIX 	256
 typedef struct {
-	int width;
-	int height;
-	frac Matrix[MAX_HEIGHT_MATRIX][MAX_WIDTH_MATRIX];
+	int 	width;
+	int 	height;
+	frac 	Matrix[MAX_HEIGHT_MATRIX][MAX_WIDTH_MATRIX];
 } matrix;
 /* wrapper struct  to  hide ugly notation and *
  * for enhancement of  the  ease of  use.  If *
@@ -17,12 +17,22 @@ typedef struct {
  * is  not recommended,  reffer  to  use  of: *
  * frac (*M)[w] where w is the width  of   M */
 
-void cpymtrx(int w, int h, frac (*A)[w], matrix *M) {
+static void cpymtrx(int w, int h, frac (*A)[w], matrix *M) {
 	for (int i = 0; i < h; i++) {
 		for (int j = 0; j < w; j++) {
 			M->Matrix[i][j].numerator = A[i][j].numerator;
 			M->Matrix[i][j].denominator = A[i][j].denominator;
 			M->Matrix[i][j].floatpoint = A[i][j].floatpoint;
+		}
+	}
+}
+
+static void cpymtrxstrct(int w, int h, matrix *F, matrix *M) {
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			M->Matrix[i][j].numerator = F->Matrix[i][j].numerator;
+			M->Matrix[i][j].denominator = F->Matrix[i][j].denominator;
+			M->Matrix[i][j].floatpoint = F->Matrix[i][j].floatpoint;
 		}
 	}
 }
@@ -47,7 +57,7 @@ void cpymtrx(int w, int h, frac (*A)[w], matrix *M) {
 /*		 class MTRX_FUNCTIONS_NO_R 		  */
 /*   print only functions with no rvalue  */
 
-void MTRX_FUNCTIONS_NO_R__addition(char *a, char *b) {
+static void MTRX_FUNCTIONS_NO_R__addition(char *a, char *b) {
 
 	int *size_A = parse_matrix_size(a);
 	int *size_B = parse_matrix_size(b);
@@ -65,7 +75,7 @@ void MTRX_FUNCTIONS_NO_R__addition(char *a, char *b) {
 	pmatrix(B, size_B[0], size_B[1], 1);
 }
 
-void MTRX_FUNCTIONS_NO_R__subtraction(char *a, char *b) {
+static void MTRX_FUNCTIONS_NO_R__subtraction(char *a, char *b) {
 	int *size_A = parse_matrix_size(a);
 	int *size_B = parse_matrix_size(b);
 
@@ -81,7 +91,7 @@ void MTRX_FUNCTIONS_NO_R__subtraction(char *a, char *b) {
 	pmatrix(B, size_B[0], size_B[1], 1);
 }
 
-void MTRX_FUNCTIONS_NO_R__multiplication(char *a, char *b) {
+static void MTRX_FUNCTIONS_NO_R__multiplication(char *a, char *b) {
 	int *size_A = parse_matrix_size(a);
 	int *size_B = parse_matrix_size(b);
 
@@ -97,7 +107,7 @@ void MTRX_FUNCTIONS_NO_R__multiplication(char *a, char *b) {
 	pmatrix(M, size_B[0], size_B[1], 1);
 }
 
-void MTRX_FUNCTIONS_NO_R__transpose(char *a) {
+static void MTRX_FUNCTIONS_NO_R__transpose(char *a) {
 	int *size_A = parse_matrix_size(a);
 
 	frac (*A)[size_A[1]] = parse_matrix(a);
@@ -108,7 +118,7 @@ void MTRX_FUNCTIONS_NO_R__transpose(char *a) {
 	pmatrix(M, size_A[0], size_A[1], 1);
 }
 
-void MTRX_FUNCTIONS_NO_R__invert(char *a) {
+static void MTRX_FUNCTIONS_NO_R__invert(char *a) {
 	int *size_A = parse_matrix_size(a);
 
 	function_assert(size_A[1] == size_A[0], 
@@ -122,7 +132,7 @@ void MTRX_FUNCTIONS_NO_R__invert(char *a) {
 	pmatrix(M, size_A[0], size_A[1], 1);
 }
 
-void MTRX_FUNCTIONS_NO_R__determinant(char *a) {
+static void MTRX_FUNCTIONS_NO_R__determinant(char *a) {
 	int *size_A = parse_matrix_size(a);
 
 	function_assert(size_A[1] == size_A[0], 
@@ -140,7 +150,7 @@ void MTRX_FUNCTIONS_NO_R__determinant(char *a) {
 	printf("\n");
 }
 
-void MTRX_FUNCTIONS_NO_R__power(char *a, int n) {
+static void MTRX_FUNCTIONS_NO_R__power(char *a, int n) {
 	int *size_A = parse_matrix_size(a);
 
 	frac (*A)[size_A[1]] = parse_matrix(a);
@@ -155,7 +165,21 @@ void MTRX_FUNCTIONS_NO_R__power(char *a, int n) {
 /*		 class MTRX_FUNCTIONS_R 		  */
 /*   functions with rvalue and str input  */
 
-matrix *MTRX_FUNCTIONS_R__addition(char *a, char *b) {
+static matrix *MTRX_FUNCTIONS_R__create(char *a) {
+	matrix *M = (matrix *) malloc(sizeof(matrix));
+
+	int *size_A = parse_matrix_size(a);
+
+	frac (*A)[size_A[1]] = parse_matrix(a);
+
+	cpymtrx(size_A[1], size_A[0], A, M);
+	M->height = size_A[0];
+	M->width = size_A[1];
+
+	return M;
+}
+
+static matrix *MTRX_FUNCTIONS_R__addition(char *a, char *b) {
 	matrix *M = (matrix *) malloc(sizeof(matrix));
 
 	int *size_A = parse_matrix_size(a);
@@ -178,7 +202,7 @@ matrix *MTRX_FUNCTIONS_R__addition(char *a, char *b) {
 	return M;
 }
 
-matrix *MTRX_FUNCTIONS_R__subtraction(char *a, char *b) {
+static matrix *MTRX_FUNCTIONS_R__subtraction(char *a, char *b) {
 	matrix *M = (matrix *) malloc(sizeof(matrix));
 
 	int *size_A = parse_matrix_size(a);
@@ -201,7 +225,7 @@ matrix *MTRX_FUNCTIONS_R__subtraction(char *a, char *b) {
 	return M;
 }
 
-matrix *MTRX_FUNCTIONS_R__multiplication(char *a, char *b) {
+static matrix *MTRX_FUNCTIONS_R__multiplication(char *a, char *b) {
 	matrix *M = (matrix *) malloc(sizeof(matrix));
 
 	int *size_A = parse_matrix_size(a);
@@ -224,7 +248,7 @@ matrix *MTRX_FUNCTIONS_R__multiplication(char *a, char *b) {
 	return M;
 }
 
-matrix *MTRX_FUNCTIONS_R__transpose(char *a) {
+static matrix *MTRX_FUNCTIONS_R__transpose(char *a) {
 	matrix *M = (matrix *) malloc(sizeof(matrix));
 
 	int *size_A = parse_matrix_size(a);
@@ -242,7 +266,7 @@ matrix *MTRX_FUNCTIONS_R__transpose(char *a) {
 	return M;
 }
 
-matrix *MTRX_FUNCTIONS_R__invert(char *a) {
+static matrix *MTRX_FUNCTIONS_R__invert(char *a) {
 	matrix *M = (matrix *) malloc(sizeof(matrix));
 
 	int *size_A = parse_matrix_size(a);
@@ -254,7 +278,7 @@ matrix *MTRX_FUNCTIONS_R__invert(char *a) {
 
 	frac _M[size_A[0]][size_A[1]];
 
-	i(size_A[0], A, _M);
+	i(size_A[1], A, _M);
 
 	cpymtrx(size_A[1], size_A[0], _M, M);
 	M->height = size_A[0];
@@ -263,7 +287,7 @@ matrix *MTRX_FUNCTIONS_R__invert(char *a) {
 	return M;
 }
 
-matrix *MTRX_FUNCTIONS_R__power(char *a, int n) {
+static matrix *MTRX_FUNCTIONS_R__power(char *a, int n) {
 	matrix *M = (matrix *) malloc(sizeof(matrix));
 
 	int *size_A = parse_matrix_size(a);
@@ -281,7 +305,7 @@ matrix *MTRX_FUNCTIONS_R__power(char *a, int n) {
 	return M;
 }
 
-int MTRX_FUNCTIONS_R__determinant(char *a) {
+static int MTRX_FUNCTIONS_R__determinant(char *a) {
 	int *size_A = parse_matrix_size(a);
 
 	function_assert(size_A[1] == size_A[0], 
@@ -301,8 +325,61 @@ int MTRX_FUNCTIONS_R__determinant(char *a) {
 /*		 class MTRX_FUNCTIONS_RAW 		  */
 /*   functions with rvalue and raw input  */
 
+static matrix *MTRX_FUNCTIONS_RAW__addition(matrix *a, matrix *b) {
+	matrix *M = (matrix *) malloc(sizeof(matrix));
 
-// . . .
+	a(a->Matrix, a->height, a->width, b->Matrix);
+
+	cpymtrxstrct(b->width, b->height, b, M);
+	M->height = b->height;
+	M->width = b->width;
+
+	return M;
+}
+
+static matrix *MTRX_FUNCTIONS_RAW__subtraction(matrix *a, matrix *b) {
+	matrix *M = (matrix *) malloc(sizeof(matrix));
+
+	s(a->Matrix, a->height, a->width, b->Matrix);
+
+	cpymtrxstrct(b->width, b->height, b, M);
+	M->height = b->height;
+	M->width = b->width;
+
+	return M;
+}
+
+static matrix *MTRX_FUNCTIONS_RAW__multiplication(matrix *a, matrix *b) {
+	matrix *M = (matrix *) malloc(sizeof(matrix));
+
+	frac _M[b->height][b->width];
+
+	m(a->Matrix, a->height, a->width, b->Matrix, _M);
+
+	cpymtrx(b->height, b->width, _M, M);
+	M->height = b->height;
+	M->width = b->width;
+
+	return M;
+}
+
+static matrix *MTRX_FUNCTIONS_RAW__transpose(matrix *a) {
+	matrix *M = (matrix *) malloc(sizeof(matrix));
+
+	frac _M[a->height][a->width];
+
+	t(a->Matrix, a->height, a->width, _M);
+
+	cpymtrx(a->height, a->width, _M, M);
+	M->height = a->height;
+	M->width = a->width;
+
+	return M;
+}
+
+// . . . 
+// TODO: add invert, power and determinant.
+
 
 /*			wrapper functions 			*/
 
@@ -341,6 +418,10 @@ void show(matrix *m) {
 }
 
 /* with rvalue */
+matrix *create(char *s) {
+	return MTRX_FUNCTIONS_R__create(s);
+}
+
 matrix *add_r(char *a, char *b) {
 	return MTRX_FUNCTIONS_R__addition(a, b);
 }
@@ -367,4 +448,20 @@ int determinant_r(char *a) {
 
 matrix *topower_r(char *a, int n) {
 	return MTRX_FUNCTIONS_R__power(a, n);
+}
+
+matrix *add_raw(matrix *a, matrix *b) {
+	return MTRX_FUNCTIONS_RAW__addition(a, b);
+}
+
+matrix *subtract_raw(matrix *a, matrix *b) {
+	return MTRX_FUNCTIONS_RAW__subtraction(a, b);
+}
+
+matrix *mulitply_raw(matrix *a, matrix *b) {
+	return MTRX_FUNCTIONS_RAW__multiplication(a, b);
+}
+
+matrix *transpose_raw(matrix *a) {
+	return MTRX_FUNCTIONS_RAW__transpose(a);
 }
