01b78cfcd5abf82c36f658c4db8a85ab3b99b034
lionholler <82542096+lionholler@users.noreply.github.com>
2021-07-30 16:02:34 +0200

    version 1

---
+++ drivers/fractional_obj_class.cpp
@@ -0,0 +1,73 @@
+#ifndef _STDIO_H
+	#include <stdio.h>
+#endif
+#ifndef _STDLIB_H
+	#include <stdlib.h>
+#endif
+
+/* very basic fractional number handling *
+ * class. For C++ projects.              *
+ * Available methods:                    *
+ *		- set(num, denom)        *	
+ *		- decimal(void)          *
+ *		- gcd(void)              *
+ *		- simplify(void)         *
+ *		- out(void)              */
+
+typedef struct {
+	long int 				numerator;
+	long int 				denominator;
+} fraction_struct;
+
+class fraction {
+	private:
+
+		fraction_struct 	*frac;
+
+		long int GCD(long int n, long int d) {
+			if (n == 0) 
+				return d;
+			return GCD(d % n, n);
+		}
+
+	public:
+
+		long int 			d; 		// denominator
+		long int 			n; 		// numerator
+		long double			dec;	        // decimal
+
+		fraction(void) {
+			frac = (fraction_struct *) malloc(sizeof(fraction_struct));
+			frac->numerator = n = 0;
+			frac->denominator = d = 1;
+			dec = 0.0;
+		}
+
+		void set(long int num, long int denom) {
+			frac->numerator = n = num;
+			frac->denominator = d = denom;
+			dec = (long double) num / denom; 
+		}
+
+		long double decimal(void) {
+			if (dec == 0.0) 
+				dec = (long double) frac->numerator / frac->denominator;
+			return dec;
+		}
+
+		long int gcd(void) {
+			return GCD(frac->numerator, frac->denominator);
+		}
+
+		void simplify(void) {
+			long int g = GCD(frac->numerator, frac->denominator);
+			set(frac->numerator / g, frac->denominator / g);
+		}
+
+		void out(void) {
+			if (d == 1) 
+				printf("%ld\n%Lf\n", n, dec);
+			else
+				printf("%ld/%ld\n%Lf\n", n, d, dec);
+		}
+};
